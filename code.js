8a43c4ad12b3ef4547c243e3bff5dfefbc708081
GURU <sahoogurucharan00@gmail.com>
2023-06-10 09:27:29 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,6 @@
 //GURU
+var username = prompt("Enter your name", "Enter username");
+if (name != null) {
+  alert("YOUR GITHUB URL - https://github.com/"+ username);
+  window.location.href=`https://heroku.com/deploy/?template=https://github.com/${username}/GURU-BOT` ;
+}
